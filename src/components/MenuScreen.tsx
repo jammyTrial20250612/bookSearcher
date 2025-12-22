47effb84095e1a915514a14e2dff6a66e5cb23f5
jam <jummy.40405202.jummy@gmail.com>
2025-12-22 18:26:40 +0900

    delete Favorite button

--- src/components/MenuScreen.tsx
+++ src/components/MenuScreen.tsx
@@ -31,13 +31,7 @@ const MenuScreen: React.FC = () => {
       title: "book search",
       description: "楽天ブックスの書籍検索APIから、タイトルで検索した書籍一覧を見れます。",
       color: "from-green-400 to-emerald-600",
-    },
-    {
-      id: pages[3],
-      title: "favorite",
-      description: "book searchで検索した書籍を、お気に入りに追加して、一覧表示で確認できる画面です。",
-      color: "from-yellow-400 to-emerald-600",
-    },
+    }
   ];
 
   return (
@@ -94,14 +88,6 @@ const MenuScreen: React.FC = () => {
                   <div className="px-4">book search</div>
                 </Link>
               </li>
-              <li>
-                <Link to="/loggedIn/books" className="flex hover:bg-yellow-400 bg-white px-4">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
-                  </svg>
-                  <div className="px-4">favorite</div>
-                </Link>
-              </li>
             </ul>
             <div className="flex items-center space-x-4">
               <div className="flex items-center space-x-3 bg-gradient-to-r from-purple-50 to-pink-50 px-4 py-2 rounded-xl">
@@ -138,7 +124,7 @@ const MenuScreen: React.FC = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {menuItems.map((item, index) => (
-            <Link to={`${item.id}`}>
+            <Link to={`${item.id}`} key={index}>
             <div
               key={index}
               // onClick={() => movePage(item)}
--- src/components/books/BookAPI.tsx
+++ src/components/books/BookAPI.tsx
@@ -79,14 +79,6 @@ const BookAPI = () => {
                   <div className="px-4">book search</div>
                 </Link>
               </li>
-              <li>
-                <Link to="/loggedIn/books" className="flex hover:bg-yellow-400 bg-white px-4">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
-                  </svg>
-                  <div className="px-4">favorite</div>
-                </Link>
-              </li>
             </ul>
             <div className="flex items-center space-x-4">
               <div className="flex items-center space-x-3 bg-gradient-to-r from-purple-50 to-pink-50 px-4 py-2 rounded-xl">
@@ -123,19 +115,8 @@ const BookAPI = () => {
               className="w-full py-4 pl-12 bg-white border border-gray-200 rounded-2xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent shadow-sm transition"
             />
             <button onClick={fetchItems} className="p-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
               </svg>
             </button>
           </div>
